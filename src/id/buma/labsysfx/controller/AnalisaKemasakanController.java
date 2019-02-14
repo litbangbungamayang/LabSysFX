0c299042e2f1841eeeead8f40abd4689ee8ea530
bayu <bayu@TP-X230>
2019-02-14 22:16:45 +0700

    GUI untuk input data analisa

--- src/id/buma/labsysfx/controller/AnalisaKemasakanController.java
+++ src/id/buma/labsysfx/controller/AnalisaKemasakanController.java
@@ -7,18 +7,21 @@ package id.buma.labsysfx.controller;
 
 import com.jfoenix.controls.JFXAutoCompletePopup;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.DoubleValidator;
 import com.jfoenix.validation.NumberValidator;
 import com.jfoenix.validation.RequiredFieldValidator;
 import id.buma.labsysfx.MainApp;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TitledPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -43,17 +46,67 @@ public class AnalisaKemasakanController implements Initializable {
     @FXML
     private Tab pageInputDataAnalisa;
     @FXML
-    private JFXComboBox<Label> cbxJenisAnalisa;
+    private Tab pageInputDataAnalisa2;
     @FXML
-    private JFXTextField txtKodePetak;
+    private JFXTextField txtPetak;
     @FXML
     private JFXTextField txtRonde;
     @FXML
-    private JFXTextField txtSampel;
+    private JFXTextField txtJenisAnalisa;
+    @FXML
+    private JFXButton btnNextAnalisa;
+    @FXML
+    private JFXTextField txtBobotTebuAtas;
+    @FXML
+    private JFXTextField txtBobotNiraAtas;
+    @FXML
+    private JFXTextField txtBobotTebuTengah;
+    @FXML
+    private JFXTextField txtBobotNiraTengah;
+    @FXML
+    private JFXTextField txtBobotTebuBawah;
+    @FXML
+    private JFXTextField txtBobotNiraBawah;
+    @FXML
+    private JFXTextField txtPenggerek;
+    @FXML
+    private JFXTextField txtBrixAtas;
+    @FXML
+    private JFXTextField txtBrixTengah;
+    @FXML
+    private JFXTextField txtBrixBawah;
+    @FXML
+    private JFXTextField txtBrixCampur;
+    @FXML
+    private JFXTextField txtPolAtas;
+    @FXML
+    private JFXTextField txtPolTengah;
+    @FXML
+    private JFXTextField txtPolBawah;
+    @FXML
+    private JFXTextField txtPolCampur;
+    @FXML
+    private JFXTextField txtSuhu;
+    @FXML
+    private JFXTextField txtKoreksiSuhu;
+    @FXML
+    private JFXTextField txtInputPanjang;
+    @FXML
+    private JFXTextField txtInputRuas;
+    @FXML
+    private JFXTextField txtInputDiameter;
+    @FXML
+    private TitledPane titPaneInputData;
+    @FXML
+    private TitledPane titPaneHasilAnalisa;
+    @FXML
+    private JFXButton btnSimpanData;
     
     private MainApp mainApp;
     
     private MainScreenController msc;
+    
+    
     
     public void setMainApp(MainApp mainApp){
         this.mainApp = mainApp;
@@ -76,6 +129,13 @@ public class AnalisaKemasakanController implements Initializable {
         validatorAngka.setMessage("Input tidak valid!");
         RequiredFieldValidator validatorWajib = new RequiredFieldValidator();
         validatorWajib.setMessage("Harus diisi!");
+        DoubleValidator validatorDesimal = new DoubleValidator();
+        validatorDesimal.setMessage("Input tidak valid! Harus angka desimal!");
+        
+        txtJenisAnalisa.getValidators().add(validatorWajib);
+        txtJenisAnalisa.focusedProperty().addListener((observable) -> {
+            txtJenisAnalisa.validate();
+        });
         
         txtRonde.getValidators().add(validatorAngka);
         txtRonde.getValidators().add(validatorWajib);
@@ -83,42 +143,179 @@ public class AnalisaKemasakanController implements Initializable {
             txtRonde.validate();
         });
         
-        txtKodePetak.getValidators().add(validatorWajib);
-        txtKodePetak.focusedProperty().addListener((observable) -> {
-            txtKodePetak.validate();
+        txtPetak.getValidators().add(validatorWajib);
+        txtPetak.focusedProperty().addListener((observable) -> {
+            txtPetak.validate();
         });
         
-        txtSampel.getValidators().add(validatorAngka);
-        txtSampel.setPromptText("1");
-        txtSampel.focusedProperty().addListener((observable) -> {
-            txtSampel.validate();
+        txtBobotTebuAtas.getValidators().add(validatorWajib);
+        txtBobotTebuAtas.getValidators().add(validatorDesimal);
+        txtBobotTebuAtas.focusedProperty().addListener((observable) -> {
+            txtBobotTebuAtas.validate();
         });
+        
+        txtBobotNiraAtas.getValidators().add(validatorWajib);
+        txtBobotNiraAtas.getValidators().add(validatorDesimal);
+        txtBobotNiraAtas.focusedProperty().addListener((observable) -> {
+            txtBobotNiraAtas.validate();
+        });
+        
+        txtBobotTebuTengah.getValidators().add(validatorWajib);
+        txtBobotTebuTengah.getValidators().add(validatorDesimal);
+        txtBobotTebuTengah.focusedProperty().addListener((observable) -> {
+            txtBobotTebuTengah.validate();
+        });
+        
+        txtBobotNiraTengah.getValidators().add(validatorWajib);
+        txtBobotNiraTengah.getValidators().add(validatorDesimal);
+        txtBobotNiraTengah.focusedProperty().addListener((observable) -> {
+            txtBobotNiraTengah.validate();
+        });
+        
+        txtBobotTebuBawah.getValidators().add(validatorWajib);
+        txtBobotTebuBawah.getValidators().add(validatorDesimal);
+        txtBobotTebuBawah.focusedProperty().addListener((observable) -> {
+            txtBobotTebuBawah.validate();
+        });
+        
+        txtBobotNiraBawah.getValidators().add(validatorWajib);
+        txtBobotNiraBawah.getValidators().add(validatorDesimal);
+        txtBobotNiraBawah.focusedProperty().addListener((observable) -> {
+            txtBobotNiraBawah.validate();
+        });
+        
+        txtPenggerek.getValidators().add(validatorWajib);
+        txtPenggerek.getValidators().add(validatorDesimal);
+        txtPenggerek.focusedProperty().addListener((observable) -> {
+            txtPenggerek.validate();
+        });
+        
+        txtBrixAtas.getValidators().add(validatorWajib);
+        txtBrixAtas.getValidators().add(validatorDesimal);
+        txtBrixAtas.focusedProperty().addListener((observable) -> {
+            txtBrixAtas.validate();
+        });
+        
+        txtBrixTengah.getValidators().add(validatorWajib);
+        txtBrixTengah.getValidators().add(validatorDesimal);
+        txtBrixTengah.focusedProperty().addListener((observable) -> {
+            txtBrixTengah.validate();
+        });
+        
+        txtBrixBawah.getValidators().add(validatorWajib);
+        txtBrixBawah.getValidators().add(validatorDesimal);
+        txtBrixBawah.focusedProperty().addListener((observable) -> {
+            txtBrixBawah.validate();
+        });
+        
+        txtBrixCampur.getValidators().add(validatorWajib);
+        txtBrixCampur.getValidators().add(validatorDesimal);
+        txtBrixCampur.focusedProperty().addListener((observable) -> {
+            txtBrixCampur.validate();
+        });
+        
+        txtPolAtas.getValidators().add(validatorWajib);
+        txtPolAtas.getValidators().add(validatorDesimal);
+        txtPolAtas.focusedProperty().addListener((observable) -> {
+            txtPolAtas.validate();
+        });
+        
+        txtPolTengah.getValidators().add(validatorWajib);
+        txtPolTengah.getValidators().add(validatorDesimal);
+        txtPolTengah.focusedProperty().addListener((observable) -> {
+            txtPolTengah.validate();
+        });
+        
+        txtPolBawah.getValidators().add(validatorWajib);
+        txtPolBawah.getValidators().add(validatorDesimal);
+        txtPolBawah.focusedProperty().addListener((observable) -> {
+            txtPolBawah.validate();
+        });
+        
+        txtPolCampur.getValidators().add(validatorWajib);
+        txtPolCampur.getValidators().add(validatorDesimal);
+        txtPolCampur.focusedProperty().addListener((observable) -> {
+            txtPolCampur.validate();
+        });
+        
+        txtSuhu.getValidators().add(validatorWajib);
+        txtSuhu.getValidators().add(validatorDesimal);
+        txtSuhu.focusedProperty().addListener((observable) -> {
+            txtSuhu.validate();
+        });
+        
+        txtKoreksiSuhu.getValidators().add(validatorWajib);
+        txtKoreksiSuhu.getValidators().add(validatorDesimal);
+        txtKoreksiSuhu.focusedProperty().addListener((observable) -> {
+            txtKoreksiSuhu.validate();
+        });
+    }
+    
+    public void autoCompleteGroup(){
+        JFXAutoCompletePopup<String> autoCompleteJenis = new JFXAutoCompletePopup<>();
+        JFXAutoCompletePopup<String> autoCompletePetak = new JFXAutoCompletePopup<>();
+        autoCompleteJenis.getSuggestions().addAll("Analisa Rutin", "Analisa Tebu Bakar", "Analisa Tebu Percobaan");
+        autoCompletePetak.getSuggestions().addAll("Rayon I Petak 056", "Rayon II Petak 129", "SARENGAT D.");// todo: tambahkan kode generate kode petak disini
+        autoCompleteJenis.cellLimitProperty().setValue(3);
+        autoCompletePetak.cellLimitProperty().setValue(10);
+        txtJenisAnalisa.textProperty().addListener((observable) -> {
+            autoCompleteJenis.filter(string -> string.toLowerCase().contains(txtJenisAnalisa.getText().toLowerCase()));
+            autoCompleteJenis.show(txtJenisAnalisa);
+        });
+        txtJenisAnalisa.focusedProperty().addListener((observable) -> {
+            if (txtJenisAnalisa.isFocused()) autoCompleteJenis.show(txtJenisAnalisa);
+        });
+        autoCompleteJenis.setSelectionHandler((evt) -> {
+            txtJenisAnalisa.setText(evt.getObject());
+        });
+        txtPetak.textProperty().addListener((observable) -> {
+            if (!txtPetak.getText().isEmpty()){
+                autoCompletePetak.filter(string -> string.toLowerCase().contains(txtPetak.getText().toLowerCase()));
+                autoCompletePetak.show(txtPetak);
+            } else {
+                autoCompletePetak.hide();
+            }
+        });
+        autoCompletePetak.setSelectionHandler((evt) -> {
+            txtPetak.setText(evt.getObject());
+            autoCompletePetak.hide();
+        });
+    }
+    
+    public void resetField(){
+        txtJenisAnalisa.clear();
+        txtPetak.clear();
+        txtRonde.clear();
+        txtPetak.getValidators().clear();
+        txtRonde.getValidators().clear();
+        
+        txtBobotNiraAtas.clear();
+        txtBobotNiraAtas.getValidators().clear();
+        
+        containerAnkem.requestFocus();
     }
         
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        validasiField();
+        
         btnSubMenuDashboard.setOnAction((event) -> {
             containerAnkem.getSelectionModel().select(pageDashboard);
         });
         btnSubMenuInputDataAnalisa.setOnAction((event) -> {
             containerAnkem.getSelectionModel().select(pageInputDataAnalisa);
-            cbxJenisAnalisa.getItems().clear();
-            cbxJenisAnalisa.getItems().add(new Label("Analisa rutin"));
-            cbxJenisAnalisa.getItems().add(new Label("Analisa tebu bakar"));
-            cbxJenisAnalisa.getItems().add(new Label("Analisa tebu percobaan"));
-            JFXAutoCompletePopup<String> autoComplete = new JFXAutoCompletePopup<>();
-            autoComplete.getSuggestions().addAll("Rayon I Petak 056", "Rayon II Petak 129", "SARENGAT D.");
-            autoComplete.setSelectionHandler((evt) -> {
-                txtKodePetak.setText(evt.getObject());
-            });
-            txtKodePetak.textProperty().addListener((observable) -> {
-                autoComplete.filter(string -> string.toLowerCase().contains(txtKodePetak.getText().toLowerCase()));
-                autoComplete.show(txtKodePetak);
-            });
+            resetField();
+            validasiField();
+            autoCompleteGroup();
+            titPaneInputData.setExpanded(true);
         });
         btnBackMainMenu.setOnAction((event) -> {
             msc.getTabPane().getSelectionModel().select(msc.getTabMainMenu());
+        });
+        btnNextAnalisa.setOnAction((event) -> {
+            containerAnkem.getSelectionModel().select(pageInputDataAnalisa2);
+        });
+        btnSimpanData.setOnAction((event) -> {
+            titPaneHasilAnalisa.setExpanded(true);
         });
     }
     
